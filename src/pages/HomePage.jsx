2c9d4746a3c94e24d432170c0c78e4cffde96cd8
VladasBog <vladas.bogdanovic@gmail.com>
2022-08-10 22:17:52 +0300

    Imported and rendered Footer component in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const HomePage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 };
 
 export default HomePage;
