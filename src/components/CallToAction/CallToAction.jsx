dee0fd0c6daf6357dfc075d9ff8683f41b255ca3
Viktorija Pravednaja <v.praved@gmail.com>
2022-08-11 22:29:37 +0300

    created and styled CTA component

---
+++ src/components/CallToAction/CallToAction.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CTA1 from "../../images/CTA1.png";
+import CTA2 from "../../images/CTA2.png";
+import CallToActionContainer from "./CallToAction.styled";
+import Button from "../Button/Button";
+
+const CallToAction = () => {
+  return (
+    <CallToActionContainer>
+      <img className="CTA1Style" src={CTA1} alt="" />
+      <div className="centerContainer">
+        <h3>Get in touch with us for your service related query</h3>
+        <Button color={false} text={"Contact us"} />
+      </div>
+      <img className="CTA2Style" src={CTA2} alt="" />
+    </CallToActionContainer>
+  );
+};
+
+export default CallToAction;
---
+++ src/components/CallToAction/CallToAction.styled.jsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+const CallToActionContainer = styled.section`
+  width: 100%;
+
+  .centerContainer {
+    height: 502px;
+    background-color: var(--primary);
+    margin-bottom: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  .CTA1Style,
+  .CTA2Style {
+    width: 100%;
+  }
+
+  h3 {
+    inline-size: 80%;
+    font-size: var(--font-size4);
+    font-weight: var(--font-weight2);
+    color: var(--white);
+    text-align: center;
+  }
+
+  @media only screen and (min-width: 728px) {
+    height: 502px;
+    position: relative;
+    display: flex;
+
+    .centerContainer {
+      position: absolute;
+      width: 45%;
+      float: left;
+      left: 20%;
+    }
+    .CTA1Style {
+      width: 50%;
+      object-fit: cover;
+    }
+    .CTA2Style {
+      width: 50%;
+      object-fit: cover;
+    }
+  }
+  @media only screen and (min-width: 1024px) {
+    .centerContainer {
+      position: absolute;
+      width: 45%;
+      float: left;
+      left: 20%;
+
+      padding: 1rem 3rem;
+      align-items: flex-start;
+    }
+
+    h3 {
+      inline-size: 90%;
+      font-size: var(--font-size3);
+      text-align: left;
+    }
+  }
+`;
+
+export default CallToActionContainer;
