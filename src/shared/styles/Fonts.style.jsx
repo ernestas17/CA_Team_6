596714e2a71f42ce43dea705aeecd58708d6a235
VladasBog <vladas.bogdanovic@gmail.com>
2022-08-14 21:09:47 +0300

    Created file to store font styles and added styles

---
+++ src/shared/styles/Fonts.style.jsx
@@ -0,0 +1,76 @@
+import styled, { css } from "styled-components";
+
+export const StyledH1 = styled.h1`
+  font-size: 64px;
+  font-weight: 800;
+  line-height: 84px;
+  letter-spacing: -0.03em;
+`;
+
+export const StyledH2 = styled.h2`
+  font-size: 58px;
+  font-weight: 800;
+  line-height: 76px;
+  letter-spacing: -0.03em;
+`;
+
+export const StyledH3 = styled.h3`
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 140%;
+  letter-spacing: -0.03em;
+`;
+export const StyledH4 = styled.h4`
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 140%;
+  letter-spacing: -0.03em;
+`;
+export const StyledH5 = styled.h5`
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 48px;
+  letter-spacing: -0.03em;
+`;
+export const StyledH6 = styled.h6`
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 150%;
+  letter-spacing: -0.03em;
+`;
+
+export const StyledTitle01 = styled.p`
+  font-size: 32px;
+  line-height: 48px;
+  font-weight: 700;
+  letter-spacing: -0.03em;
+`;
+
+export const StyledTitle02 = styled.p`
+  @media screen and (min-width: 1024px) {
+    font-size: 24px;
+    line-height: 36px;
+    font-weight: 600;
+    letter-spacing: -0.03em;
+  }
+`;
+
+export const StyledBody1 = styled.p`
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 175%;
+  opacity: 0.6;
+`;
+
+export const StyledBody2 = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 28px;
+  opacity: 0.6;
+`;
+
+export const StyledLinkFont = css`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 28px;
+`;
