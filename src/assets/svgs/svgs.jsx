4fac5e410b386c240fe99f7260800e44e13cb220
VladasBog <vladas.bogdanovic@gmail.com>
2022-08-22 01:14:33 +0300

    Added checkmark icon

--- src/assets/svgs/svgs.jsx
+++ src/assets/svgs/svgs.jsx
@@ -661,3 +661,23 @@ export const MinusIcon = (props) => {
     </svg>
   );
 };
+
+export const CheckmarkIcon = (props) => {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="24" cy="24" r="24" fill="#00D289" />
+      <path
+        d="M12 24.5L19 31.5L36.5 14"
+        stroke="white"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
