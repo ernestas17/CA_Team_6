d746ddd9bed86b6a0ed3a99d6b78ea5725aa7cff
VladasBog <vladas.bogdanovic@gmail.com>
2022-08-22 01:17:29 +0300

    Created ServicesPage component with styles

--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+// Components
+import HowFirst from "./components/HowFirst/HowFirst";
+import HowSecond from "./components/HowSecond/HowSecond";
+import Services from "./components/Services/Services";
+import ServicesHeader from "./components/ServicesHeader/ServicesHeader";
+import CallToAction from "../../components/CallToAction";
+
+// Styles
+import StyledBackground from "./ServicesPage.style";
 
 const ServicesPage = () => {
-  return <div>ServicesPage</div>;
+  return (
+    <main>
+      <StyledBackground color={"#fff"}>
+        <ServicesHeader />
+      </StyledBackground>
+
+      <StyledBackground color={"#F5F5F5"}>
+        <Services />
+      </StyledBackground>
+
+      <StyledBackground>
+        <HowFirst />
+      </StyledBackground>
+
+      <StyledBackground>
+        <HowSecond />
+      </StyledBackground>
+
+      <CallToAction />
+    </main>
+  );
 };
 
 export default ServicesPage;
--- src/pages/ServicesPage/ServicesPage.style.jsx
+++ src/pages/ServicesPage/ServicesPage.style.jsx
@@ -1,1 +1,7 @@
 import styled from "styled-components";
+
+export const StyledBackground = styled.section`
+  background-color: ${({ color }) => color};
+`;
+
+export default StyledBackground;
