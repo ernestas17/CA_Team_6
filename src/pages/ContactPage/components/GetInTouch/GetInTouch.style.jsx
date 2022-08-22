134ed763ea92c62e8985e6dfaf9d64606acbc038
VladasBog <vladas.bogdanovic@gmail.com>
2022-08-22 16:22:45 +0300

    Created GetInTouch component with styles + export

---
+++ src/pages/ContactPage/components/GetInTouch/GetInTouch.style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div``;
+
+export const StyledContentWrapper = styled.div`
+  margin-bottom: 40px;
+  @media screen and (min-width: 768px) {
+    max-width: 624px;
+  }
+`;
