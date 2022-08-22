0a474cf4d988093393394b150ebee205e590e8e4
VladasBog <vladas.bogdanovic@gmail.com>
2022-08-22 17:52:14 +0300

    Moved AboutPage component to AboutPage folder

---
+++ src/pages/AboutPage/AboutPage.style.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledMaintenante = styled.h1`
+  font-size: 30px;
+  color: #f4f4f4;
+  background-color: #9e2a2b;
+  border-radius: 8px;
+  text-align: center;
+  padding: 20px;
+  margin: 20px 80px;
+`;
