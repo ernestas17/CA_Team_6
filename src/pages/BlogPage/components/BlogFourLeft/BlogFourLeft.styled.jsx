e7eb76cce363fa65bbc6b97fcf3f75e20b56f137
Viktorija Pravednaja <v.praved@gmail.com>
2022-09-01 12:24:31 +0300

    created  blogpage component with style

---
+++ src/pages/BlogPage/components/BlogFourLeft/BlogFourLeft.styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+// Styles
+import { StyledH4 } from "../../../../shared/styles/Fonts.style";
+
+export const StyledHeadline = styled(StyledH4)`
+  font-size: 40px;
+  margin-bottom: 16px;
+`;
