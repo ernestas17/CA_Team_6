363faf47b511cfeef5037053574bd6ae11c3f8bd
Viktorija Pravednaja <v.praved@gmail.com>
2022-09-07 15:02:27 +0300

    added shadow effect for button

--- src/components/button/Button.styled.jsx
+++ src/components/button/Button.styled.jsx
@@ -7,11 +7,11 @@ const ButtonStyle = styled.button`
   border: none;
   background-color: ${(props) =>
     props.color ? "var(--primary)" : "var(--black)"};
-
   color: var(--white);
   font-size: var(--font-size7);
   &:hover {
     cursor: pointer;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.3);
   }
 `;
 
